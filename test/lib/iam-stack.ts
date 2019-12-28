c12974a5ef2296d0aa000545299811a2a88b7ea8
curry9999 <curry123456789@gmail.com>
2019-12-29 01:01:20 +0900

    init test

---
+++ test/lib/iam-stack.ts
@@ -0,0 +1,22 @@
+import cdk = require('@aws-cdk/core');
+import iam = require('@aws-cdk/aws-iam');
+import { ManagedPolicy } from '@aws-cdk/aws-iam';
+
+export class IamStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const iamGroup = new iam.Group(this, 'IamGroup', {
+      groupName: 'iam-group-billing',
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('job-function/Billing')
+      ]
+    });
+
+    const iamUser = new iam.User(this, 'IamUser', {
+      userName: 'iam-user-billing'
+    });
+
+    iamGroup.addUser(iamUser);
+  }
+}
---
+++ test/test/iam.test.ts
@@ -0,0 +1,16 @@
+import cdk = require('@aws-cdk/core');
+import Iam = require('../lib/iam-stack');
+import '@aws-cdk/assert/jest';
+
+test('IamStack Tests', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Iam.IamStack(app, 'IamStack');
+    // THEN
+    expect(stack).toHaveResource('AWS::IAM::Group', {
+      "GroupName": "iam-group-billing",
+    });
+    expect(stack).toHaveResource('AWS::IAM::User', {
+      "UserName": "iam-user-billing",
+    });
+});
