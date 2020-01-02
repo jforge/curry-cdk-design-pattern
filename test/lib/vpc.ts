9d07c06a7ef1d203033777a0a0509da00def48fc
curry9999 <curry123456789@gmail.com>
2020-01-03 01:54:17 +0900

    Revemo SubnetType

--- test/lib/vpc.ts
+++ test/lib/vpc.ts
@@ -1,5 +1,5 @@
 import cdk = require('@aws-cdk/core');
-import { SubnetType, Vpc } from '@aws-cdk/aws-ec2'
+import { Vpc } from '@aws-cdk/aws-ec2'
 
 export class VpcStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
