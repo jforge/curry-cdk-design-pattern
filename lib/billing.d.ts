ab68b40b6c15456daa1662e985d68b6f535b8d44
curry9999 <curry123456789@gmail.com>
2020-01-30 03:47:52 +0900

    add .gitignore

--- lib/billing.d.ts
+++
@@ -1,4 +0,0 @@
-import cdk = require('@aws-cdk/core');
-export declare class IamStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
--- lib/vpc.d.ts
+++
@@ -1,7 +0,0 @@
-import cdk = require('@aws-cdk/core');
-export interface VpcProps extends cdk.StackProps {
-    maxAzs: number;
-}
-export declare class VpcStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props: VpcProps);
-}
